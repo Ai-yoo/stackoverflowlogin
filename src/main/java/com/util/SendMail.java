bb1a58e0d9512ff0ffa3d02b4b7e7e63ae27b246
Ai-yoo <duzhentong123@foxmail.com>
2018-06-08 16:10:58 +0800

    修改邮件格式

--- src/main/java/com/util/SendMail.java
+++ src/main/java/com/util/SendMail.java
@@ -7,6 +7,7 @@ import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
 import java.util.Date;
 import java.util.Properties;
@@ -40,16 +41,16 @@ public class SendMail {
             msg.setSubject("错误报告");
             // 设置邮件内容
             StringBuffer strBody = new StringBuffer();
-            strBody.append("大哥！！！出错了！！！登不上了！！！</br>");
-            strBody.append("<font size=\"9\" style=\"font-weight:bold;\">别玩了！！！</font>");
+            strBody.append("糟糕！！！程序出错了！！！登不上了！！！</br>");
+            strBody.append("<font size=\"7\" style=\"font-weight:bold;\">别玩了！！！</font>");
             //163  18846054508邮箱
 //            msg.setText(strBody.toString());
             msg.setContent(strBody.toString(),"text/html;charset=UTF-8");
             msg.setSentDate(new Date());
-            msg.setFrom(new InternetAddress("18846054508@163.com"));
+            msg.setFrom(new InternetAddress("18846054508@163.com", "stackoverflow自动登录程序", "utf-8"));
 
             transport = session.getTransport();
-            transport.connect("smtp.163.com", "18846054508@163.com", "邮箱授权码");
+            transport.connect("smtp.163.com", "18846054508@163.com", "163shouquanma");
 
             transport.sendMessage(msg, new Address[] { new InternetAddress("1958321982@qq.com") });
         } catch (GeneralSecurityException e) {
@@ -60,7 +61,9 @@ public class SendMail {
             e.printStackTrace();
         } catch (MessagingException e) {
             e.printStackTrace();
-        }finally {
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } finally {
             try {
                 transport.close();
             } catch (MessagingException e) {
