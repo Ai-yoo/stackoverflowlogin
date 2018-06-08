131dc090ff0f5d161765ff5cabeb3058be8a2830
Ai-yoo <duzhentong123@foxmail.com>
2018-06-08 16:12:01 +0800

    修改获取用户信息方式

--- src/main/java/com/util/GetUser.java
+++ src/main/java/com/util/GetUser.java
@@ -4,6 +4,8 @@ import com.duzhentong.Login;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -14,18 +16,20 @@ import java.util.Properties;
  * @Time 18:54
  */
 public class GetUser {
-    public static String[] getUser() {
-        String[] user = new String[2];
+    public static Map<String,String> getUser() {
+        Map<String, String> userMemages = new HashMap<String, String>();
         Properties properties = new Properties();
         try {
             InputStream inputStream = Login.class.getClassLoader()
                     .getResourceAsStream("user.properties");
             properties.load(inputStream);
-            user[0] = properties.getProperty("email").trim();
-            user[1] = properties.getProperty("password").trim();
+            String email = properties.getProperty("email").trim();
+            String password = properties.getProperty("password").trim();
+            userMemages.put("email", email);
+            userMemages.put("password", password);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return user;
+        return userMemages;
     }
 }
